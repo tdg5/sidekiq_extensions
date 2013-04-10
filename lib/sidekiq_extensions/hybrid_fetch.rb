5d2cb1301a30f0db32733b5e3e0deaa958cbd813
Danny Guinther <dannyguinther@gmail.com>
2013-04-10 08:00:12 -0400

    Make sure SidekiqExtensions module exists

--- lib/sidekiq_extensions/hybrid_fetch.rb
+++ lib/sidekiq_extensions/hybrid_fetch.rb
@@ -1,17 +1,20 @@
-module SidekiqExtensions::PriorityFetch
+module SidekiqExtensions
 
-	def initialize(options)
-		@strictly_ordered_queues = !!options[:strict]
-		@queues = options[:queues]
-		@priority_queues = options[:priority_queues]
-		[@queues, @priority_queues].map{|queues| queues.map {|q| "queue:#{q}"}}
-		@unique_queues = (@priority_queues + @queues).uniq
-	end
+	module PriorityFetch
+
+		def initialize(options)
+			@strictly_ordered_queues = !!options[:strict]
+			@queues = options[:queues]
+			@priority_queues = options[:priority_queues]
+			[@queues, @priority_queues].map{|queues| queues.map {|q| "queue:#{q}"}}
+			@unique_queues = (@priority_queues + @queues).uniq
+		end
 
 
-	def queues_cmd
-		queues = @strictly_ordered_queues ? @unique_queues.dup : (@priority_queues + @queues.shuffle).uniq
-		queues << Sidekiq::Fetcher::TIMEOUT
+		def queues_cmd
+			queues = @strictly_ordered_queues ? @unique_queues.dup : (@priority_queues + @queues.shuffle).uniq
+			queues << Sidekiq::Fetcher::TIMEOUT
+		end
 	end
 
 end
