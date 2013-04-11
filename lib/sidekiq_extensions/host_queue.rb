83504549d0155bd09429cd0cb888508e48883ca8
Danny Guinther <dannyguinther@gmail.com>
2013-04-11 07:05:11 -0400

    Remove automatic host_queue addition in favor of register_host_queue method

--- lib/sidekiq_extensions/host_queue.rb
+++ lib/sidekiq_extensions/host_queue.rb
@@ -6,6 +6,16 @@ module Sidekiq
 		return options[:host_queue] || Socket.gethostname
 	end
 
+
+	def self.register_host_queue(weight = 1)
+		weight = 1 unless weight.is_a?(Fixnum) && weight > 0
+		existing_weight = options[:queues].select{|queue| queue == host_queue}.length
+		needed_weight = weight - existing_weight
+		return unless needed_weight > 0
+		needed_weight.times do
+			options[:queues] << host_queue
+		end
+	end
+
 end
 
-Sidekiq.options[:queues] << Sidekiq.host_queue unless Sidekiq.options[:queues].include?(Sidekiq.host_queue)
