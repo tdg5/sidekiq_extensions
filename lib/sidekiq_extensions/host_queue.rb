2a7d76fc9cdf4e7368fac55ca2abe12ff5c4906f
Danny Guinther <dannyguinther@gmail.com>
2013-04-13 06:42:47 -0400

    Don't extend Sidekiq namespace

--- lib/sidekiq_extensions/host_queue.rb
+++ lib/sidekiq_extensions/host_queue.rb
@@ -1,19 +1,19 @@
 require 'socket'
 
-module Sidekiq
+module SidekiqExtensions
 
 	def self.host_queue
-		return options[:host_queue] || Socket.gethostname
+		return Sidekiq.options[:host_queue] || Socket.gethostname
 	end
 
 
 	def self.register_host_queue(weight = 1)
 		weight = 1 unless weight.is_a?(Fixnum) && weight > 0
-		existing_weight = options[:queues].select{|queue| queue == host_queue}.length
+		existing_weight = Sidekiq.options[:queues].select{|queue| queue == host_queue}.length
 		needed_weight = weight - existing_weight
 		return unless needed_weight > 0
 		needed_weight.times do
-			options[:queues] << host_queue
+			Sidekiq.options[:queues] << host_queue
 		end
 	end
 
