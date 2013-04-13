535fa66c25ec9c4750af01b88da9d21b31f5c355
Danny Guinther <dannyguinther@gmail.com>
2013-04-13 09:56:47 -0400

    Split calculation of insetion_index from prioritize_queue

--- lib/sidekiq_extensions/priority_queue.rb
+++ lib/sidekiq_extensions/priority_queue.rb
@@ -4,25 +4,25 @@ module SidekiqExtensions
 
 	def self.prioritize_queue(queue_name, position = {})
 		raise ArgumentError, "Invalid queue name of class #{queue_name.class}" unless [String, Symbol].include?(queue_name.class)
-		queue = queue_name.to_s
-		if position.empty?
-			remove_queue(queue, :from => :priority_queues)
-			Sidekiq.options[:priority_queues].unshift(queue)
-		else
-			unless position.length == 1 && [:before, :after].include?(positioning = position.keys[0])
-				raise ArgumentError, 'Invalid position argument. Position expects only one option with a key of :before or :after'
-			end
-			reference_queue = position.values[0].to_s
-			raise ArgumentError, 'Queue cannot be positioned relative to itself' if reference_queue == queue
-			reference_position = Sidekiq.options[:priority_queues].index(reference_queue)
-			raise "Unable to find reference priority queue #{reference_queue}" unless reference_position
-			existing_position = Sidekiq.options[:priority_queues].index(queue)
-			reference_position -= 1 if existing_position < reference_position
-			remove_queue(queue, :from => :priority_queues)
-			reference_position += 1 if positioning == :after
-			Sidekiq.options[:priority_queues].insert(reference_position, queue)
+		unless position.empty? || position.length == 1 && [:before, :after].include?(position.keys[0])
+			raise ArgumentError, 'Invalid position argument. Position expects only one option with a key of :before or :after'
 		end
+		queue = queue_name.to_s
+		insert_at = insertion_index(queue, position.keys[0], position.values[0].to_s)
+		remove_queue(queue, :from => :priority_queues)
+		Sidekiq.options[:priority_queues].insert(insert_at, queue)
 		remove_queue(queue)
+	end
+
+	private
+	def self.insertion_index(queue, position = nil, reference_queue = nil)
+		return 0 if position.nil? || reference_queue.empty?
+		raise ArgumentError, 'Queue cannot be positioned relative to itself' if reference_queue == queue
+		reference_position = Sidekiq.options[:priority_queues].index(reference_queue)
+		raise "Unable to find reference priority queue #{reference_queue}" unless reference_position
+		existing_position = Sidekiq.options[:priority_queues].index(queue)
+		reference_position -= 1 if existing_position && existing_position < reference_position
+		return position == :after ? reference_position + 1 : reference_position
 	end
 
 end
